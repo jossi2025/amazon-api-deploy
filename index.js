d6eeefcc684791632ac441aca1ad0fe0890a773d
jossi2025 <jossihabtetsion10@gmail.com>
2025-03-09 17:42:24 +0200

    fixed

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 // const logger = require("firebase-functions/logger");
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
-const stripe = require("stripe")(process.env.STRIP_KEY);
+require('dotenv').config();
+console.log('Stripe Secret Key:', process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 app.use(cors({ origin: true }));
